faaf948329541fb452d6bf4fd0c147540eebb664
Saull Brandão <saull@outlook.com>
2022-10-24 10:05:59 -0300

    templates

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent {
+  nomePortal: string;
 
+  cursos: string[] = ['Java', 'Angular', 'Ext JS']
+
+  constructor() {
+    this.nomePortal = 'https://loiane.training'
+  }
 }
